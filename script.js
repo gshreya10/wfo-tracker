734021fc82ef142494896cd3cbd68b2bc9e77764
gshreya <guptashreya1008@gmail.com>
2026-05-18 15:52:12 +0530

    Initial project setup

---
+++ script.js
@@ -0,0 +1,10 @@
+const loginBtn = document.getElementById("login-btn");
+const dashboard = document.getElementById("dashboard");
+
+loginBtn.addEventListener("click", () => {
+
+    alert("Google Login Coming Next");
+
+    dashboard.classList.remove("hidden");
+
+});
